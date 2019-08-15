eb831a43e58a4881525171f4a77d1654929706cb
Anthony Gamboa <anthony.b.gamboa@gmail.com>
2019-08-15 15:07:58 -0700

    added circle drawing

--- js/index.js
+++ js/index.js
@@ -1,9 +1,8 @@
-var firstPoint = new Point(20, 40);
-var secondPoint = new Point(firstPoint);
-console.log(secondPoint); // { x: 20, y: 40 }
-
-secondPoint.y = 20;
-console.log(secondPoint); // { x: 20, y: 20 }
-
-// Note that firstPoint has not changed:
-console.log(firstPoint); // { x: 20, y: 40 }
+function onMouseUp(event) {
+	var circle = new Path.Circle({
+		center: event.middlePoint,
+		radius: event.delta.length / 2
+	});
+	circle.strokeColor = 'black';
+	circle.fillColor = 'white';
+}
