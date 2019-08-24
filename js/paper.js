4ca9528470cea05af78ed51efb08cfc5fe1fffde
Anthony Gamboa <anthony.b.gamboa@gmail.com>
2019-08-24 13:11:25 -0700

    Added touch event handling. Now simplify pen strokes. Changed pen stroke cap to round.

--- js/paper.js
+++ js/paper.js
@@ -15,170 +15,213 @@ function onMouseDown(event) {
 	project.deselectAll();
 	segment = path = null;
 	var hitResult = project.hitTest(event.point, hitOptions);
-	switch (event.event.buttons) {
-		//left drag
-		case 1:
-			if (hitResult) {
-				path = hitResult.item;
-				switch (hitResult.type) {
-					case 'segment':
-						segment = hitResult.segment;
-						break;
-					case 'stroke':
-						if (globals.function == "select") {
-							var location = hitResult.location;
-							segment = path.insert(location.index + 1, event.point);
-						}
-						break;
-					default:
-						break;
-				}
-			}
 
-			switch (globals.function) {
-				case "select":
-					if (event.item != null)
-						event.item.set({ selected: true });
-					break;
-				case "pen":
-					penPath = new Path();
-					penPath.strokeColor = globals.strokeColor;
-					penPath.strokeWidth = globals.strokeWidth;
-					break;
-				case "brush":
-					brushPath = new Path();
-					brushPath.fillColor = globals.strokeColor;
-					brushPath.add(event.point);
-					break;
-				case "circle":
-					break;
-				default:
-					break;
-			}
+	//if it's a mouse event
+	if (!!event.event.buttons) {
+		switch (event.event.buttons) {
+			//left click
+			case 1:
+				leftClickDown(hitResult, event);
+				break;
+				//right click
+			case 2:
+				break;
+				//middle click
+			case 4:
+				break;
+		}
+	}
+
+	//if it's a touch event
+	if (event.event.type === "touchstart") {
+		leftClickDown(hitResult, event);
+	}
+}
+
+function leftClickDown(hitResult, event) {
+	if (hitResult) {
+		path = hitResult.item;
+		switch (hitResult.type) {
+			case 'segment':
+				segment = hitResult.segment;
+				break;
+			case 'stroke':
+				if (globals.function == "select") {
+					var location = hitResult.location;
+					segment = path.insert(location.index + 1, event.point);
+				}
+				break;
+			default:
+				break;
+		}
+	}
+
+	switch (globals.function) {
+		case "select":
+			if (event.item != null)
+				event.item.set({
+					selected: true
+				});
 			break;
-		//right drag
-		case 2:
+		case "pen":
+			penPath = new Path();
+			penPath.strokeCap = 'round';
+			penPath.strokeColor = globals.strokeColor;
+			penPath.strokeWidth = globals.strokeWidth;
 			break;
-		//middle drag
-		case 4:
+		case "brush":
+			brushPath = new Path();
+			brushPath.fillColor = globals.strokeColor;
+			brushPath.add(event.point);
+			break;
+		case "circle":
+			break;
+		default:
 			break;
 	}
 }
 
 function onMouseDrag(event) {
-	switch (event.event.buttons) {
-		//left drag
-		case 1:
-			switch (globals.function) {
-				case "pan":
-					moveCenter(event);
-					break;
-				case "select":
-					if (segment) {
-						segment.point += event.delta;
-					}
-					else if (path) {
-						path.position += event.delta;
-					}
-					break;
-				case "pen":
-					penPath.add(event.point);
-					break;
-				case "brush":
-					var step = event.delta;
-					step.angle += 90;
+	//if it's a click event
+	if (!!event.event.buttons) {
+		switch (event.event.buttons) {
+			//left drag
+			case 1:
+				leftDrag(event);
+				break;
+				//right drag
+			case 2:
+				break;
+				//middle drag
+			case 4:
+				moveCenter(event);
+				$("#myCanvas").addClass("grabby");
+				break;
+		}
+	}
 
-					var top = event.middlePoint + (step * (globals.strokeWidth / 30) + 1);
-					var bottom = event.middlePoint - (step * (globals.strokeWidth / 30) + 1);
+	//if it's a touch event
+	if (event.event.type === "touchmove") {
+		leftDrag(event);
+	}
+}
 
-					var line = new Path();
-					line.strokeColor = globals.strokeColor;;
-					line.add(top);
-					line.add(bottom);
-
-					brushPath.add(top);
-					brushPath.insert(0, bottom);
-					brushPath.smooth();
-					break;
-				case "circle":
-					break;
-				default:
-					break;
+function leftDrag(event) {
+	switch (globals.function) {
+		case "pan":
+			moveCenter(event);
+			break;
+		case "select":
+			if (segment) {
+				segment.point += event.delta;
+			} else if (path) {
+				path.position += event.delta;
 			}
 			break;
-		//right drag
-		case 2:
+		case "pen":
+			penPath.add(event.point);
 			break;
-		//middle drag
-		case 4:
-			moveCenter(event);
-			$("#myCanvas").addClass("grabby");
+		case "brush":
+			var step = event.delta;
+			step.angle += 90;
+
+			var top = event.middlePoint + (step * (globals.strokeWidth / 30) + 1);
+			var bottom = event.middlePoint - (step * (globals.strokeWidth / 30) + 1);
+
+			var line = new Path();
+			line.strokeColor = globals.strokeColor;;
+			line.add(top);
+			line.add(bottom);
+
+			brushPath.add(top);
+			brushPath.insert(0, bottom);
+			brushPath.smooth();
+			break;
+		case "circle":
+			break;
+		default:
 			break;
 	}
 }
 
 function onMouseUp(event) {
-	switch (event.event.button) {
-		//left click
-		case 0:
-			switch (globals.function) {
-				case "line":
-					var linePath = new Path();
-					linePath.strokeColor = globals.strokeColor;
-					linePath.strokeWidth = globals.strokeWidth;
-					linePath.add(event.downPoint);
-					linePath.add(event.point);
-					break;
-				case "brush":
-					brushPath.add(event.point);
-					brushPath.closed = true;
-					brushPath.smooth();
-					break;
-				case "circle":
-					var circle = new Path.Circle({
-						center: event.middlePoint,
-						radius: event.delta.length / 2
-					});
-					circle.strokeColor = globals.strokeColor;
-					circle.fillColor = globals.fillColor;
-					circle.strokeWidth = globals.strokeWidth;
-					break;
-				case "ellipse":
-					var rectangle = new Rectangle({
-						x: event.point.x - event.delta.x,
-						y: event.point.y - event.delta.y,
-						height: event.delta.y,
-						width: event.delta.x
-					});
-					var ellipse = new Path.Ellipse(
-						rectangle
-					);
-					ellipse.strokeColor = globals.strokeColor;
-					ellipse.fillColor = globals.fillColor;
-					ellipse.strokeWidth = globals.strokeWidth;
-					break;
-				case "rectangle":
-					var rectangle = new Path.Rectangle({
-						x: event.point.x - event.delta.x,
-						y: event.point.y - event.delta.y,
-						height: event.delta.y,
-						width: event.delta.x
-					});
-					rectangle.strokeColor = globals.strokeColor;
-					rectangle.fillColor = globals.fillColor;
-					rectangle.strokeWidth = globals.strokeWidth;
-					break;
-				default:
-					break;
-			}
+	//if it's a mouse event
+	if (!!event.event.buttons) {
+		switch (event.event.button) {
+			//left click
+			case 0:
+				leftClickUp(event);
+				break;
+				//right drag
+			case 2:
+				break;
+				//middle drag
+			case 1:
+				if (globals.function != "pan")
+					$("#myCanvas").removeClass("grabby");
+				break;
+		}
+	}
+
+	//if it's a touch event
+	if (event.event.type === "touchend") {
+		leftClickUp(event);
+	}
+}
+
+function leftClickUp(event) {
+	switch (globals.function) {
+		case "line":
+			var linePath = new Path();
+			linePath.strokeColor = globals.strokeColor;
+			linePath.strokeWidth = globals.strokeWidth;
+			linePath.add(event.downPoint);
+			linePath.add(event.point);
 			break;
-		//right drag
-		case 2:
+		case "pen":
+			penPath.simplify();
 			break;
-		//middle drag
-		case 1:
-			if (globals.function != "pan")
-				$("#myCanvas").removeClass("grabby");
+		case "brush":
+			brushPath.add(event.point);
+			brushPath.closed = true;
+			// brushPath.simplify(10);
+			brushPath.smooth();
+			break;
+		case "circle":
+			var circle = new Path.Circle({
+				center: event.middlePoint,
+				radius: event.delta.length / 2
+			});
+			circle.strokeColor = globals.strokeColor;
+			circle.fillColor = globals.fillColor;
+			circle.strokeWidth = globals.strokeWidth;
+			break;
+		case "ellipse":
+			var rectangle = new Rectangle({
+				x: event.point.x - event.delta.x,
+				y: event.point.y - event.delta.y,
+				height: event.delta.y,
+				width: event.delta.x
+			});
+			var ellipse = new Path.Ellipse(
+				rectangle
+			);
+			ellipse.strokeColor = globals.strokeColor;
+			ellipse.fillColor = globals.fillColor;
+			ellipse.strokeWidth = globals.strokeWidth;
+			break;
+		case "rectangle":
+			var rectangle = new Path.Rectangle({
+				x: event.point.x - event.delta.x,
+				y: event.point.y - event.delta.y,
+				height: event.delta.y,
+				width: event.delta.x
+			});
+			rectangle.strokeColor = globals.strokeColor;
+			rectangle.fillColor = globals.fillColor;
+			rectangle.strokeWidth = globals.strokeWidth;
+			break;
+		default:
 			break;
 	}
 }
